52e373df16f2bf0ad0d78b17f11083f9864ae48c
Ashish Verma <ashish1500616@gmail.com>
2017-06-11 20:52:48 +0530

    add edit and add post

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 
 ]
 if settings.DEBUG:
